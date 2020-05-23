5d0d3f3401b744bae4112e9c2e5dbf062a7df1e3
tintinmovie <31559358+tintinmovie@users.noreply.github.com>
2020-05-23 14:21:28 +1200

    Remove wandb

--- code/ProjectPaintingLight.py
+++ code/ProjectPaintingLight.py
@@ -132,7 +132,7 @@ def generate_lighting_effects(stroke_density, content):
 def run(image, mask, ambient_intensity, light_intensity, light_source_height, gamma_correction, stroke_density_clipping, light_color_red, light_color_green, light_color_blue, enabling_multiple_channel_effects):
 
 
-    wandb.init(entity='ayush-thakur', project='paintlight')
+    #wandb.init(entity='ayush-thakur', project='paintlight')
     
     # Some pre-processing to resize images and remove input JPEG artifacts.
     raw_image = min_resize(image, 512)
